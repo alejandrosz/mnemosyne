e7e0e0c0080141c8eed63054a45238f9feb84d7e
alejandrosz <alejandrosz@gmail.com>
2020-03-05 16:06:02 +0100

    first deploy motherfuckers

---
+++ server/public/precache-manifest.6aacea8799eabb5d8b9deae7207713fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c83fb40d47e89fa6315e",
+    "url": "/static/js/main.c83fb40d.chunk.js"
+  },
+  {
+    "revision": "5e650b2d6c5e9d462615",
+    "url": "/static/js/1.5e650b2d.chunk.js"
+  },
+  {
+    "revision": "c83fb40d47e89fa6315e",
+    "url": "/static/css/main.cd6e0bf3.chunk.css"
+  },
+  {
+    "revision": "f6a390e5e51ff5ecd2cf33d52386b122",
+    "url": "/index.html"
+  }
+];
