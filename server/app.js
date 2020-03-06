dda24c267b19a8d07d29fc7bad3833ee1e8a31ba
alejandrosz <alejandrosz@gmail.com>
2020-03-06 10:49:04 +0100

    fixing cors problem with dani 5

--- server/app.js
+++ server/app.js
@@ -74,7 +74,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');    
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Auth-Token, X-Requested-With, Content-Type');   
-  res.setHeader('Access-Control-Allow-Credentials', true);    
+  res.setHeader('Access-Control-Allow-Credentials', "true");    
   next();
 });
 
