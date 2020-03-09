9a4fd132930a8eda21563ef041d5b3c83e3efbad
alejandrosz <alejandrosz@gmail.com>
2020-03-09 14:09:02 +0100

    hola

---
+++ server/public/precache-manifest.004a5051f5a27b3f3f73b0912ea5cef6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bf7025e220baffbb3bb8",
+    "url": "/static/js/main.bf7025e2.chunk.js"
+  },
+  {
+    "revision": "35dba4a5919c9da39333",
+    "url": "/static/js/1.35dba4a5.chunk.js"
+  },
+  {
+    "revision": "bf7025e220baffbb3bb8",
+    "url": "/static/css/main.0fbbeacf.chunk.css"
+  },
+  {
+    "revision": "0fa5c5b1ff9cad6907cdd56a34e30df4",
+    "url": "/index.html"
+  }
+];
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9f99fe807f04882e3ebe4fe2040b7c1.js"
+  "/precache-manifest.004a5051f5a27b3f3f73b0912ea5cef6.js"
 );
 
 workbox.clientsClaim();
