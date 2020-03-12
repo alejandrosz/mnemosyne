138cb04b0ca1df78b7ad6ae94c20f51c1ab5776a
alejandrosz <alejandrosz@gmail.com>
2020-03-12 18:52:32 +0100

    almost done

--- client/src/InitialCard/InitialCard.js
+++ client/src/InitialCard/InitialCard.js
@@ -8,7 +8,62 @@ class InitialCard extends Component {
   render() {
     return (
       <div className="initialcard">
-        hola <button onClick={this.props.closeInitialCard}>Close</button>
+        <button onClick={this.props.closeInitialCard}>&#8213;</button>
+        <p>
+          <span className="bold-text">mnemosine</span> is a tool for browsing
+          media databases{" "}
+        </p>
+        <br />
+        <p>
+          At the beggining, random sample of the archive from the
+          Metropolitan Museum of Art, Museum of Modern Art and Rijksmuseum of
+          Amsterdam are showed. In total there are more than 1.230.000 pieces of art
+          reachable through their APIs and databases.
+        </p>
+        <br />
+        <p>
+          You can use the search bar in combination with the selector of tags
+          and the range of years to find more results according to your
+          interests.
+        </p>
+        <br />
+        <ul>
+          <li>The results are nested in 5 levels:</li>
+          <li>
+            <span className="bold-text">1</span> Database to which they belong
+          </li>
+          <li>
+            <span className="bold-text">2</span> Year of creation
+          </li>
+          <li>
+            <span className="bold-text">3</span> Geographical origin
+          </li>
+          <li>
+            <span className="bold-text">4</span> Author
+          </li>
+          <li>
+            <span className="bold-text">5</span> Single piece
+          </li>
+        </ul>
+        <br />
+        <p>
+          In the last level, you can open a new tab and get full info of the
+          piece.
+        </p>
+        <br />
+        <p>
+          Sign up and create your own collections adding pieces to them.
+        </p>
+        <br />
+        <p>
+          You can like any item! They are showed bigger when they gain
+          likes.
+        </p>
+        <br />
+        <p>
+          The aim of the project is make possible connect more and more media
+          databases.
+        </p>
       </div>
     );
   }
