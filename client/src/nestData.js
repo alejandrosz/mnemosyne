aa2e3bada169a72c65603d0ed0fc3bbf17465916
alejandrosz <alejandrosz@gmail.com>
2020-03-13 10:51:57 +0100

    el readme

--- client/src/nestData.js
+++ client/src/nestData.js
@@ -22,6 +22,34 @@ export function nestByMuseum(pieces) {
   return tree;
 }
 
+// script para comprimir las imagenes del rijks
+// compress(e) {
+//   const width = 500;
+//   const height = 300;
+//   const fileName = e.target.files[0].name;
+//   const reader = new FileReader();
+//   reader.readAsDataURL(e.target.files[0]);
+//   reader.onload = event => {
+//       const img = new Image();
+//       img.src = event.target.result;
+//       img.onload = () => {
+//               const elem = document.createElement('canvas');
+//               elem.width = width;
+//               elem.height = height;
+//               const ctx = elem.getContext('2d');
+//               // img.width and img.height will contain the original dimensions
+//               ctx.drawImage(img, 0, 0, width, height);
+//               ctx.canvas.toBlob((blob) => {
+//                   const file = new File([blob], fileName, {
+//                       type: 'image/jpeg',
+//                       lastModified: Date.now()
+//                   });
+//               }, 'image/jpeg', 1);
+//           },
+//           reader.onerror = error => console.log(error);
+//   };
+// }
+
 export function nestByDate(museumPieces) {
   const mainDates = getDatesFromPieces(museumPieces);
   const nestedMuseum = [];
